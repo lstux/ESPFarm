c109b6f45ef95e41dfb866a311a164f538e648cd
eric@lslinux.org <eric@lslinux.org>
2024-02-14 11:20:04 +0100

    feat all hardware libs working

--- include/config.h
+++ include/config.h
@@ -1,6 +1,19 @@
 #define SERIAL_SPEED 115200
 
-#define PIN_I2CSDA 6
-#define PIN_I2CSCL 7
+#define SPI_SD_CS_PIN 5
 
-#define PIN_SD_CS 5
+#define I2C_SDA_PIN      6
+#define I2C_SCL_PIN      7
+#define I2C_ADDR_U8G2    0x3c
+#define I2C_ADDR_BME280  0x76
+#define I2C_ADDR_DS3231  0x68
+#define I2C_ADDR_ADS1115 0x48
+
+
+#define ADS1115_GAIN  1 // 0=6.144 1=4.096 2=2.048 4=1.024 8=0.512 16=0.256V
+#define ADS1115_MODE  1 // 0=continuous, 1=single reading
+#define ADS1115_RATE  0 // 0=8 1=16 2=32 3=64 4=128 5=250 6=475 7=860Hz
+
+#define LIGHT_CHANNEL 0
+#define LIGHT_MIN     10500
+#define LIGHT_MAX     100
--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,21 @@
 #include "config.h"
 #include "i2cscanner.h"
 
-
-#include <U8g2lib.h>
-U8G2_SH1106_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
-bool u8g2_initialized = false;
-
-#include <SparkFunBME280.h>
-BME280 bme280;
-bool bme280_initialized = false;
-
 #include <SD.h>
 bool SD_initialized = false;
+
+#include <U8g2lib.h>
+U8G2_SH1106_128X64_NONAME_1_HW_I2C *u8g2 = nullptr;
+
+#include <RTClib.h>
+RTC_DS3231 *ds3231 = nullptr;
+
+#include <SparkFunBME280.h>
+BME280 *bme280;
+
+#include <ADS1X15.h>
+ADS1115 *ads1115 = nullptr;
+
 
 void i2c_scan() {
   int e;
@@ -61,30 +65,31 @@ void wifi_scan() {
 }
 
 void bme280_test() {
-  if (!bme280_initialized) return;
+  if (bme280 == nullptr) return;
   Serial.println();
   Serial.println(F("*** BME280 readings ***"));
-  Serial.print(F("  Temperature "));  Serial.println(bme280.readTempC(), 2);
-  Serial.print(F("  Humidity ")); Serial.println(bme280.readFloatHumidity(), 0);
-  Serial.print(F("  Pressure ")); Serial.println(bme280.readFloatPressure()/100, 0);
+  Serial.print(F("  Temperature "));  Serial.println(bme280->readTempC(), 2);
+  Serial.print(F("  Humidity ")); Serial.println(bme280->readFloatHumidity(), 0);
+  Serial.print(F("  Pressure ")); Serial.println(bme280->readFloatPressure()/100, 0);
   return;
 }
 
 
 void u8g2_test() {
-  if (!u8g2_initialized) return;
-  u8g2.firstPage();
+  if (u8g2 == nullptr) return;
+  u8g2->firstPage();
   do {
     /*u8g2.drawHLine(1,1,10);
     u8g2.drawHLine(1+5,2,5);*/
-    u8g2.setFont(u8g2_font_ncenB10_tr);
-    u8g2.drawStr(2,24,"Hello World!");
-  } while ( u8g2.nextPage() );
+    u8g2->setFont(u8g2_font_ncenB10_tr);
+    u8g2->drawStr(2,24,"Hello World!");
+  } while ( u8g2->nextPage() );
   return;
 }
 
 void sd_test() {
   if (!SD_initialized) return;
+  Serial.println();
   Serial.println(F("*** SD card ***"));
   switch(SD.cardType()) {
     case CARD_NONE: Serial.println(F("  No card found"));    return; break;
@@ -97,30 +102,76 @@ void sd_test() {
   return;
 }
 
+void ds3231_test() {
+  if (ds3231 == nullptr) return;
+  Serial.println();
+  Serial.println(F("*** DS3231 RTC ***"));
+  DateTime now = ds3231->now();
+  Serial.print(F("  Date : "));
+  Serial.print(now.year(), DEC); Serial.print("/"); Serial.print(now.month(), DEC); Serial.print("/"); Serial.println(now.day());
+  Serial.print(F("  Time : "));
+  Serial.print(now.hour(), DEC); Serial.print(':'); Serial.print(now.minute(), DEC); Serial.print(':'); Serial.println(now.second(), DEC);
+  return;
+}
+
+void ads1115_test() {
+  if (ads1115 == nullptr) return;
+  Serial.println();
+  Serial.println(F("*** ADS1115 ADC ***"));
+  int16_t v, m;
+  for (uint8_t i=0; i<4; i++) {
+    v = ads1115->readADC(i); m = map(v, 0, 32768, 0, 100);
+    /* m = ((float)v/32768)*100; */
+    Serial.print(F("  channel ")); Serial.print(i); Serial.print(F(" : "));
+    Serial.print(v); Serial.print(F("raw (")); Serial.print(m); Serial.println(F("%)"));
+  }
+  v = ads1115->readADC(LIGHT_CHANNEL); m = map(v, LIGHT_MIN, LIGHT_MAX, 0, 100);
+  /*m = (int16_t)((((float)v-LIGHT_MIN)/(LIGHT_MAX-LIGHT_MIN))*100);
+  if (m>100) m=100;*/
+  Serial.print(F("  light   ")); Serial.print(LIGHT_CHANNEL); Serial.print(F(" : "));
+  Serial.print(v); Serial.print(F("raw (")); Serial.print(m); Serial.println(F("%)"));
+  return;
+}
+
 void setup() {
   Serial.begin(SERIAL_SPEED);
-  delay(500);
+  delay(2000);
   Serial.println(F("ESPFarm booting"));
-  Wire.begin(PIN_I2CSDA, PIN_I2CSCL);
+  Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN);
 
-  if (SD.begin(PIN_SD_CS)) {
+  /*** SD card setup ***/
+  if (SD.begin(SPI_SD_CS_PIN)) {
     SD_initialized = true;
     Serial.println(F("SD card initialized"));    
   }
   else { Serial.println(F("Error : SD initialization failed")); }
 
-  if (u8g2.begin()) {
-    u8g2_initialized = true;
-    Serial.println(F("U8g2 screen initialized"));
-  }
-  else { Serial.println(F("Error : U8g2 screen initialization failed")); }
+  /*** U8G2 LCD Screen setup ***/
+  u8g2 = new U8G2_SH1106_128X64_NONAME_1_HW_I2C(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+  if (u8g2->begin()) { Serial.println(F("U8g2 screen initialized")); }
+  else { Serial.println(F("Error : U8g2 screen initialization failed")); free(u8g2); u8g2 = nullptr; }
 
-  bme280.setI2CAddress(0x76);
-  if (bme280.beginI2C()) {
-    bme280_initialized = true;
-    Serial.println(F("BME280 initialized"));
+  /*** DS3231 RTC setup ***/
+  ds3231 = new RTC_DS3231();
+  if (ds3231->begin()) {
+    Serial.println(F("DS3231 RTC initialized"));
+    if (ds3231->lostPower()) {
+      Serial.println(F("Warning : RTC lost power, setting to firmware compilation time"));
+      ds3231->adjust(DateTime(F(__DATE__), F(__TIME__)));
+    }
   }
-  else { Serial.println(F("BME280 initialization failed")); }
+  else { Serial.println(F("Error : DS3231 RTC initialization failed")); free(ds3231); ds3231 = nullptr; }
+
+  /*** BME280 sensor setup ***/
+  bme280 = new BME280();
+  bme280->setI2CAddress(I2C_ADDR_BME280);
+  if (bme280->beginI2C()) { Serial.println(F("BME280 initialized")); }
+  else { Serial.println(F("BME280 initialization failed")); free(bme280); bme280 = nullptr; }
+
+  /** ADS1115 ADC setup ***/
+  ads1115 = new ADS1115(I2C_ADDR_ADS1115);
+  if (ads1115->begin()) { Serial.println(F("ADS1115 ADC initialized")); ads1115->setGain(ADS1115_GAIN); ads1115->setMode(ADS1115_MODE); ads1115->setDataRate(ADS1115_RATE); }
+  else { Serial.println(F("Error : ADS1115 ADC initialization failed")); free(ads1115); ads1115 = nullptr; }
 
   delay(2000);
 }
@@ -129,7 +180,9 @@ void loop() {
   static uint8_t i=0;
   sd_test();
   i2c_scan();
+  ds3231_test();
   bme280_test();
+  ads1115_test();
   u8g2_test();
   if ((i%10)==0) { wifi_scan(); i=0; }
   i++;
